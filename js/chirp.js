db2c6115309e45cdabc3610e3c3f693e4a91f14a
Tyler <tylertaydus4444@gmail.com>
2019-01-16 20:17:01 -1000

    passing

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,29 @@
+
+let call = '';
+
 function chirp(n){
-  // FIX ME
+  
+  if(n===0){
+    call = '';
+    return;
+  } 
+
+  console.log('n before:', n);
+  chirp(--n);
+  console.log('n after:',n); 
+  call = call + ' chirp';
+  console.log(call);
+  return call;
 }
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
 });
+
+
+// var addChirp = ' chirp';
+// var finalChirp = makeChirp.concat(addChirp);
+// console.log('finalChirp:',finalChirp)
+
+  //arrChirp.push('chirp');
+  //console.log(arrChirp);
